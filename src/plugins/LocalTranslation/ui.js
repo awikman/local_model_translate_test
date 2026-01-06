d25df55e6443df07e31a118e74780ef73b7dadea
Aleksi Wikman <aleksi.wikman@gmail.com>
2026-01-06 17:54:49 -0300

    better icons

--- src/plugins/LocalTranslation/ui.js
+++ src/plugins/LocalTranslation/ui.js
@@ -19,7 +19,7 @@ export default class TranslationUI extends Plugin {
       dropdownView.buttonView.set({
         label: 'Translate',
         tooltip: 'Translate to selected language',
-        withText: false
+        withText: true
       });
 
       dropdownView.buttonView.icon = this._getTranslateIcon();
@@ -76,6 +76,6 @@ export default class TranslationUI extends Plugin {
   }
 
   _getTranslateIcon() {
-    return '<svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2L3 7v11l7 5 7-5V7l-7-5z"/></svg>';
+    return '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="currentColor"><path d="M12.87 15.07l-2.54-2.51.03-.03c1.74-1.94 2.98-4.17 3.71-6.53H17V4h-7V2H8v2H1v2h11.17C11.5 7.92 10.44 9.75 9 11.35 8.07 10.32 7.3 9.19 6.69 8h-2c.73 1.63 1.73 3.17 2.98 4.56l-5.09 5.02L4 19l5-5 3.11 3.11.76-2.04zM18.5 10h-2L12 22h2l1.12-3h4.75L21 22h2l-4.5-12zm-2.62 7l1.62-4.33L19.12 17h-3.24z"/></svg>';
   }
 }
